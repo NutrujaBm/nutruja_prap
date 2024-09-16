076470d3151153c84029999c54ff0f166a531c69
Boombm <nutruja.bm@gmail.com>
2024-09-16 09:08:32 +0700

    fix page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,80 +1,82 @@
-// function toggleMenu() {
-//     const menu = document.querySelector(".menu-links");
-//     const icon = document.querySelector(".hamburger-icon");
-//     menu.classList.toggle("open")
-//     icon.classList.toggle("open")
-// }
-
-// function showInfo(section) {
-//     const sections = document.querySelectorAll('.info-content');
-//     sections.forEach(sec => sec.style.display = 'none');
-    
-//     document.getElementById(section).style.display = 'block';
-// }
-
-/* MIXITUP FILTER PORTFOLIO */
-let mixerPortfolio = mixitup('.work-container', {
-    selectors: {
-        target: '.work-card'
-    },
-    animation: {
-        duration: 300
-    }
+// Initialize MixItUp for the portfolio filter
+let mixerPortfolio = mixitup(".work-container", {
+  selectors: {
+    target: ".work-card",
+  },
+  animation: {
+    duration: 300,
+  },
 });
 
-/* LINK ACTIVE WORK */
-const linkWork = document.querySelectorAll('.work-item')
+// Function to handle active link for work items
+const linkWork = document.querySelectorAll(".work-item");
 
 function activeWork() {
-    linkWork.forEach(L=> L.classList.remove('active-work'))
-    this.classList.add('active-work')
+  linkWork.forEach((L) => L.classList.remove("active-work"));
+  this.classList.add("active-work");
 }
 
-linkWork.forEach(L=> L.addEventListener("click", activeWork))
+linkWork.forEach((L) => L.addEventListener("click", activeWork));
 
-/* WORK POPUP */
+// Function to toggle the portfolio popup
 document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("work-button")) {
-      togglePortfolioPopup();
-      portfolioItemDetails(e.target.parentElement);
-    }
+  if (e.target.classList.contains("work-button")) {
+    togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
 });
-  
+
 function togglePortfolioPopup() {
-    document.querySelector(".portfolio-popup").classList.toggle("open");
-}
-  
-document.querySelector(".portfolio-popup-close").addEventListener("click", togglePortfolioPopup);
-  
-function portfolioItemDetails(portfolioItem) {
-    document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(".work-img").src;
-    document.querySelector(".portfolio-popup-subtitle span").innerHTML = portfolioItem.querySelector(".work-title").innerHTML;
-    document.querySelector(".portfolio-popup-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+  document.querySelector(".portfolio-popup").classList.toggle("open");
 }
 
-/* INPUT ANIMATION */
+document
+  .querySelector(".portfolio-popup-close")
+  .addEventListener("click", togglePortfolioPopup);
+
+// Function to set portfolio item details
+function portfolioItemDetails(portfolioItem) {
+  document.querySelector(".pp-thumbnail img").src =
+    portfolioItem.querySelector(".work-img").src;
+  document.querySelector(".portfolio-popup-subtitle span").innerHTML =
+    portfolioItem.querySelector(".work-title").innerHTML;
+  document.querySelector(".portfolio-popup-body").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+}
+
+// Function for input focus animation
 const inputs = document.querySelectorAll(".input");
 
-function focusFunc(){
-    let parent = this.parentNode;
-    parent.classList.add("focus");
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
 }
 
-function blurFunc(){
-    let parent = this.parentNode;
-    if(this.value == "") {
-        parent.classList.remove("focus");
-    }
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
 }
 
 inputs.forEach((input) => {
-    input.addEventListener("focus", focusFunc);
-    input.addEventListener("blur", blurFunc);
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
 });
 
+// Function to handle tab switching
+function openTab(tabName) {
+  const tabs = document.querySelectorAll(".tab-content");
+  const links = document.querySelectorAll(".tab-links");
 
+  tabs.forEach((tab) => {
+    tab.classList.remove("active-tab");
+  });
 
+  links.forEach((link) => {
+    link.classList.remove("active-link");
+  });
 
-
-
-
+  document.getElementById(tabName).classList.add("active-tab");
+  event.target.classList.add("active-link");
+}
